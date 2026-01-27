161c31d8d53a2d0fa811101ede7d758748d7c4ad
dorcax <dorcasibrahim667@gmail.com>
2026-01-27 06:11:31 +0100

    fixed

--- src/hooks/useAuthRedirect.ts
+++ src/hooks/useAuthRedirect.ts
@@ -1,50 +1,3 @@
-// "use client";
-
-// import { useRouter } from "next/navigation";
-// import { useMe } from "@/lib/queries/auth/useMe";
-// import { useProfile } from "@/lib/queries/user/useProfile";
-
-// export function useAuthRedirect() {
-//   const router = useRouter();
-//   const { data: user, isLoading: userLoading } = useMe();
-//   const profileId = user?.profile?.id
-//   console.log("uuuuuu",user)
-//   console.log("userme profile",profileId)
-//   const { data: profile, isLoading: profileLoading } = useProfile(profileId);
-
-//   const redirect = () => {
-//     if (userLoading || profileLoading) return;
-
-//     if (!user) {
-//       router.replace("/auth/sign-in");
-//       return;
-//     }
-
-//     if (!profile || !profile.name) {
-//       router.replace("/auth/profile-setup");
-//       return;
-//     }
-
-//     if (user.role === "CURATOR") {
-//       router.replace("/listers/inventory");
-//       return;
-//     }
-
-//     if (user.role === "DRESSER") {
-//       router.replace("/shop");
-//       return;
-//     }
-//   };
-
-//   return {
-//     redirect,
-//     isReady: !userLoading && !profileLoading,
-//   };
-// }
-
-
-
-
 "use client";
 
 import { useRouter } from "next/navigation";
@@ -54,27 +7,24 @@ import { useProfile } from "@/lib/queries/user/useProfile";
 export function useAuthRedirect() {
   const router = useRouter();
   const { data: user, isLoading: userLoading } = useMe();
-
-  // Wait for user to load, then get profileId
-  const profileId = user?.profile?.id;
-
-  // Fetch profile only when profileId exists
+  const profileId = user?.profile?.id
+  console.log("uuuuuu",user)
+  console.log("userme profile",profileId)
   const { data: profile, isLoading: profileLoading } = useProfile(profileId);
 
   const redirect = () => {
-    if (userLoading || profileLoading) return; // wait for both
+    if (userLoading || profileLoading) return;
 
     if (!user) {
       router.replace("/auth/sign-in");
       return;
     }
 
-    if (!profileId) {
+    if (!profile || !profile.name) {
       router.replace("/auth/profile-setup");
       return;
     }
 
-    // Redirect by role
     if (user.role === "CURATOR") {
       router.replace("/listers/inventory");
       return;
@@ -91,3 +41,6 @@ export function useAuthRedirect() {
     isReady: !userLoading && !profileLoading,
   };
 }
+
+
+
