e3445a0e87942efaec9141f47a8308cc1aa1fa64
Chisom <nwikechisom77@gmail.com>
2026-01-31 12:42:34 +0100

    ...

--- src/app/listers/layout.tsx
+++ src/app/listers/layout.tsx
@@ -15,23 +15,23 @@ export default function CuratorsLayout({ children }: { children: ReactNode }) {
   const { data: user, isLoading: userLoading } = useMe();
   const { data: profile, isLoading: profileLoading } = useProfile();
 
-  // useEffect(() => {
-  //   if (!userLoading && !profileLoading) {
-  //     if (!user) {
-  //       router.replace("/auth/sign-in");
-  //     } else if (!profile) {
-  //       router.replace("/auth/profile-setup");
-  //     }
-  //   }
-  // }, [user, profile, userLoading, profileLoading, router]);
+  useEffect(() => {
+    if (!userLoading && !profileLoading) {
+      if (!user) {
+        router.replace("/auth/sign-in");
+      } else if (!profile) {
+        router.replace("/auth/profile-setup");
+      }
+    }
+  }, [user, profile, userLoading, profileLoading, router]);
 
-  // if (userLoading || profileLoading) {
-  //   return <FullPageLoader />;
-  // }
+  if (userLoading || profileLoading) {
+    return <FullPageLoader />;
+  }
 
-  // if (!user || !profile) {
-  //   return <FullPageLoader />;
-  // }
+  if (!user || !profile) {
+    return <FullPageLoader />;
+  }
 
   return (
     <>
